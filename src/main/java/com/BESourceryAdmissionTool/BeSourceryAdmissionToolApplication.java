1ca20e83fea3a6137a1a13d2cb0872846fc56c7b
Tomas Matijoška <tomas.matijoska@gmail.com>
2022-10-29 12:58:36 +0300

    initial project commit

---
+++ src/main/java/com/BESourceryAdmissionTool/BeSourceryAdmissionToolApplication.java
@@ -0,0 +1,13 @@
+package com.BESourceryAdmissionTool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeSourceryAdmissionToolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeSourceryAdmissionToolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/BESourceryAdmissionTool/BeSourceryAdmissionToolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.BESourceryAdmissionTool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeSourceryAdmissionToolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
