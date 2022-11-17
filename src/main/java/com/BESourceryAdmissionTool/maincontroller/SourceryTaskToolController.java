aaa4935045f4e6a3e359935204b37ca71efc1521
Mykolas <mykolas.kucinskas2@gmail.com>
2022-11-17 13:53:18 +0200

    feat: added swagger and Swagger UI, created a simple OK endpoint

--- src/main/java/com/BESourceryAdmissionTool/maincontroller/SourceryTaskToolController.java
+++ src/main/java/com/BESourceryAdmissionTool/maincontroller/SourceryTaskToolController.java
@@ -1,15 +1,16 @@
 package com.BESourceryAdmissionTool.maincontroller;
 
 
+import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api")
 public class SourceryTaskToolController {
 
     @GetMapping("/ok")
+    @ApiOperation(value = "Returns an OK response 200")
     public String getOkResponse() {
         return "Ok Response";
     }
