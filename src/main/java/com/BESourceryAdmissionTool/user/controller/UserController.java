02443cb69f31556d87e28d1c31620d46e36b9255
Karolina Ramaskaite <karolina.ramaskaite@devbridge.com>
2022-12-08 17:15:21 +0200

    gitlab config fixes, added cors config

--- src/main/java/com/BESourceryAdmissionTool/user/controller/UserController.java
+++ src/main/java/com/BESourceryAdmissionTool/user/controller/UserController.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Optional;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:3000")
 @RequestMapping("users")
 public class UserController {
     private final UserService userService;
