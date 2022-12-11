02b2458349e30b6b0a38e532827474678aefa2f6
titas Dragūnas <titas.drag@gmail.com>
2022-12-11 16:40:58 +0200

    Delleted unused encoder

--- src/main/java/com/BESourceryAdmissionTool/user/config/SecurityConfiguration.java
+++ src/main/java/com/BESourceryAdmissionTool/user/config/SecurityConfiguration.java
@@ -1,13 +1,11 @@
 package com.BESourceryAdmissionTool.user.config;
 
-import com.BESourceryAdmissionTool.user.security.PasswordEncoderModel;
 import com.BESourceryAdmissionTool.user.repositories.UserRepository;
 import com.BESourceryAdmissionTool.user.security.JwtAuthFilter;
 import com.BESourceryAdmissionTool.user.security.JwtMaker;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
