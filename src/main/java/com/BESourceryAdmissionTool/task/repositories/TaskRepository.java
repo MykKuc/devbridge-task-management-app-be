00deb6c92a456792dc207ef8c2fba51c6e27b203
Haroldas Bartusevičius <bartuseviciusharoldas@gmail.com>
2022-12-15 21:46:07 +0200

    Create statistics endpoint

--- src/main/java/com/BESourceryAdmissionTool/task/repositories/TaskRepository.java
+++ src/main/java/com/BESourceryAdmissionTool/task/repositories/TaskRepository.java
@@ -3,6 +3,7 @@ package com.BESourceryAdmissionTool.task.repositories;
 import com.BESourceryAdmissionTool.answer.model.Answer;
 import com.BESourceryAdmissionTool.category.model.Category;
 import com.BESourceryAdmissionTool.task.model.Task;
+import com.BESourceryAdmissionTool.task.projection.TaskStatistics;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -20,5 +21,6 @@ public interface TaskRepository extends JpaRepository<Task, Long> {
     Optional<Task> findTaskByTitle(String title);
     List<Task> findTasksByAuthorId(long authorId);
 
-
+    @Query("select c.name as category, COUNT(t) as taskCount from Category c left join Task t on t.category.id = c.id group by c.name")
+    List<TaskStatistics> findTaskStatistics();
 }
